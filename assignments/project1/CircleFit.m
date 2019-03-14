4b52f246a13cae5bf7d227a66160e1a5afc26aa5
manuelbre <bmanuel@ethz.ch>
2019-03-14 11:25:12 +1100

    Cleaned code and speed up circle fitting.

--- assignments/project1/CircleFit.m
+++ assignments/project1/CircleFit.m
@@ -1,4 +1,18 @@
 function [C, r_squared] = CircleFit(P)
+    %% CircleFit
+    % Manuel Breitenstein, 14.03.2019
+    %
+    % Fit a circle for 2D points by solving a linear system of equations.
+    %
+    % INPUT:
+    %       - P (N x 2): 2D Points used for fitting the circle onto. First
+    %                    column contains X-coordinates, second columnt
+    %                    y-coordinates.
+    %
+    % OUTPUT:
+    %       - C (1 x 2): Center of Circle.
+    %       - r_squared (1 x 1): Squared radius of Circle.
+    %%
    X = P(:, 1);
    Y = P(:, 2);
     
@@ -7,7 +21,7 @@ function [C, r_squared] = CircleFit(P)
      
    B = [ ( X(2:end).^2 - X(1:end-1).^2) + ( Y(2:end).^2 - Y(1:end-1).^2)];
    
-   C = (pinv(A) * B);
+   C = A\B;
    
    r_squared = sum((X-C(1)).^2 + (Y-C(2)).^2 , 1)/ (size(X,1));
    C = C.';
